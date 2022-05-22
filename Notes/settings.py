a5dd3f01ab47a14941929b6b167f203a57a12d39
abir101 <abir101@pop-os.localdomain>
2022-05-22 21:23:11 +0530

    Deployment

--- Notes/settings.py
+++ Notes/settings.py
@@ -1,13 +1,15 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cwexka%o&dc77nh$k!*8)9a&o*ho_ank6ulh0s%xy0w+&)%z1s'
+# SECRET_KEY = 'django-insecure-cwexka%o&dc77nh$k!*8)9a&o*ho_ank6ulh0s%xy0w+&)%z1s'
+SECRET_KEY = os.environ.get( 'DJANGO_SECRET_KEY', 'c@^%&+bm93*6h)ee44ewp(nyb6p9t(beu55)q2qip%g+na-&1a' )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get( 'DJANGO_DEBUG', 'True' )
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -29,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -103,9 +106,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles';
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
