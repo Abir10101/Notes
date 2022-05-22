b80b9de2bbe5b1a7e2791d32fc5ffa54934c41b7
abir101 <abir101@pop-os.localdomain>
2022-05-22 22:53:47 +0530

    Deployment

--- Notes/settings.py
+++ Notes/settings.py
@@ -6,14 +6,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if os.environ.get('DJANGO_SECRET_KEY'):
-    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 else:
     SECRET_KEY = 'c@^%&+bm93*6h)ee44ewp(nyb6p9t(beu55)q2qip%g+na-&1a'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('DJANGO_DEBUG'):
-    DEBUG = os.environ['DJANGO_DEBUG']
+    DEBUG = os.environ.get('DJANGO_DEBUG')
 else:
     DEBUG = True
 
